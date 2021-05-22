3af56abe6d81e6fc716f62e3e1dcb678159fe23a
eunbi-go <55976889+eunbi-go@users.noreply.github.com>
2021-05-22 19:37:07 +0900

    박스 오피스 순위 윈도우 생성

--- MainApp.py
+++ MainApp.py
@@ -1,21 +1,32 @@
 from tkinter import *
 from tkinter import font
+import tkinter.ttk
 import tkinter.messagebox
 
 mainWnd = Tk()
 mainWnd.geometry("600x600")
+mainWnd.title("영화 정보 검색 앱")
 
-def InitTopText():
-    Font = font.Font(mainWnd, size=20, weight='bold', family='Consolas')
-    mainText = Label(mainWnd, font=Font, text='영화 정보 검색 앱')
-    mainText.pack()
-    mainText.place(x=30)
+class MainGUI:
+    def __init__(self):
+        Font = font.Font(mainWnd, size=20, weight='bold', family='Consolas')
+        mainText = Label(mainWnd, font=Font, text='영화 정보 검색 앱')
+        mainText.pack()
+        mainText.place(x=30)
 
-    mainImg = tkinter.PhotoImage(file="movie0.png", master=mainWnd)
-    mainImgLabel = Label(mainWnd, image=mainImg)
-    mainImgLabel.pack()
-    mainImgLabel.place(x=10, y=50)
+        self.SetButton()
+        mainWnd.mainloop()
 
+    def SetButton(self):
+        tmpFont = font.Font(mainWnd, size=15, weight='bold', family='Consolas')
+        self.boxOffice = Button(mainWnd, text='박스 오피스 순위', font=tmpFont, command=self.BoxOffice, width=15, height=5)
+        self.boxOffice.place(x=0, y=40)
 
-InitTopText()
-mainWnd.mainloop()
+    def BoxOffice(self):
+        self.BoxOfficeWnd = Tk()
+        self.BoxOfficeWnd.geometry("1000x500")
+        self.BoxOfficeWnd.title("박스 오피스 순위")
+        self.BoxOfficeWnd.mainloop()
+        pass
+
+MainGUI()
