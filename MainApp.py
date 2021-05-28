669577351b0fe15a30a2cf9b1886a6bc7cf4d8d4
eunbi-go <55976889+eunbi-go@users.noreply.github.com>
2021-05-28 23:27:16 +0900

    영화검색 ui 배치중

--- MainApp.py
+++ MainApp.py
@@ -5,6 +5,8 @@ import tkinter.messagebox
 import requests
 import json
 from PIL import Image
+from io import BytesIO
+import webbrowser
 from SearchMovie import *
 from SearchActor import *
 
@@ -12,7 +14,6 @@ mainWnd = Tk()
 mainWnd.geometry("900x600")
 mainWnd.title("영화 정보 검색 앱")
 mainWnd.configure(bg='black')
-
 
 Font = font.Font(mainWnd, size=20, weight='bold', family='Consolas')
 
@@ -24,10 +25,16 @@ global photo
 photo = PhotoImage(file='box.png', master=frameBoxOffice)
 notebook.add(frameBoxOffice, image=photo)
 
+
 frame2 = Frame(mainWnd)
 global photo2
 photo2 = PhotoImage(file='movie0.png', master=frameBoxOffice)
 notebook.add(frame2, image=photo2)
+
+movieNmEt = Entry(frame2, bd=5)
+movieNmEt.pack()
+movieNmEt.place(x=100,y=100, width=100,height=40)
+
 
 class BoxOfficeRank:
     def __init__(self, frame):
@@ -42,8 +49,6 @@ class BoxOfficeRank:
         self.BoxOfficeWnd.config(bg='white')
         tmpFont = font.Font(self.BoxOfficeWnd, size=20, weight='bold', family='Consolas')
 
-        frame = Frame(self.BoxOfficeWnd)
-        frame.pack()
         global yearEt, monthEt, dayEt
         yearEt = Entry(self.BoxOfficeWnd, bd=5, selectborderwidth=1)
         monthEt = Entry(self.BoxOfficeWnd, bd=5)
@@ -291,21 +296,205 @@ class BoxOfficeRank:
         self.labelDayCnt3.config(text=self.showCnt[self.dayRankIdx+2])
 
 
+class SearchMovie:
+    def __init__(self, frame):
+        self.mainWnd = frame
+        self.movieCnt = 0
 
-if notebook.select(frameBoxOffice) != True:
-    BoxOfficeRank(frameBoxOffice)
-elif notebook.select(frame2) != True:
-    SearchMovie(frame2)
+        self.movieNmEt = Entry(frame2, bd=5)
+        self.movieNmEt.pack()
+        self.movieNmEt.place(x=110,y=30, width=100,height=40)
+
+        searchBt = Button(frame2, font=('Courier',15), text='검색',
+                          command=self.search)
+        searchBt.pack()
+        searchBt.place(x=220,y=30)
+
+        # 정보 보기 버튼
+        self.infoBt = Button(self.mainWnd, font=('Courier',15), text='정보보기',
+                             command=self.showInfo)
+        self.infoBt.place(x=290,y=30)
+
+        # 아이콘 이미지
+        global photo
+        photo = PhotoImage(file='movie.gif', master=self.mainWnd)
+        photoL = Label(self.mainWnd, image=photo)
+        photoL.pack()
+        photoL.place(x=0,y=0)
+        l1 = Label(self.mainWnd, text='영화 제목 검색', font=('Courier',20))
+        l1.pack()
+        l1.place(x=110,y=0)
+
+        # 영화 정보 표기
+        self.movieListbox = Listbox(self.mainWnd, width=25,height=18, relief='solid')
+        self.movieListbox.pack()
+        self.movieListbox.place(x=0,y=90)
+
+        # 개봉일
+        dateL = Label(self.mainWnd, text='개봉년도', font=("Courier",15))
+        dateL.place(x=200,y=200)
+        # 평점
+        ratingL = Label(self.mainWnd, text='평점', font=("Courier",15))
+        ratingL.place(x=200,y=230)
+        # 장르
+        genreL = Label(self.mainWnd, text='장르', font=('Courier',15))
+        genreL.place(x=400,y=230)
+        # 감독
+        directorL = Label(self.mainWnd, text='감독', font=("Courier",15))
+        directorL.place(x=200,y=260)
+        # 출연배우
+        actorsL = Label(self.mainWnd, text='출연배우', font=("Courier",15))
+        actorsL.place(x=200,y=290)
+
+        # 개봉일
+        self.labelDate = Label(self.mainWnd, font=("Courier",15), text=' ')
+        self.labelDate.pack()
+        self.labelDate.place(x=300,y=200)
+        # 평점
+        self.labelRate = Label(self.mainWnd, font=("Courier",15), text=' ')
+        self.labelRate.pack()
+        self.labelRate.place(x=300,y=230)
+        # 장르
+        self.labelGenre = Label(self.mainWnd, font=('Courier',10), text=' ')
+        self.labelGenre.pack()
+        self.labelGenre.place(x=450,y=230)
+        # 감독
+        self.labelDirector = Label(self.mainWnd, font=("Courier",15), text=' ')
+        self.labelDirector.pack()
+        self.labelDirector.place(x=300,y=260)
+        # 출연배우
+        self.labelActors = Label(self.mainWnd, font=("Courier",10), text=' ')
+        self.labelActors.pack()
+        self.labelActors.place(x=300,y=290)
+
+        self.labelActors2 = Label(self.mainWnd, font=("Courier",10), text=' ')
+        self.labelActors2.pack()
+        self.labelActors2.place(x=300,y=320)
+
+        self.mainWnd.mainloop()
+
+    def search(self):
+        self.strSearch = self.movieNmEt.get()
+        self.movieListbox.delete(0, END)
+
+        # 네이버 openAPI 읽어오기
+        client_id = "tvo5aUWG9rwBq1YRMqyJ"
+        client_secret = "40VkT1fuAS"
+        header_parms ={"X-Naver-Client-Id":client_id,"X-Naver-Client-Secret":client_secret}
+        url = f"https://openapi.naver.com/v1/search/movie.json?query={self.strSearch}"
+        res=requests.get(url,headers=header_parms)
+
+        self.Alldata = res.json()
+        self.movieCnt = len(self.Alldata['items'])
+        self.title = []
+        self.naverlink = []
+        self.image = []
+        self.date = []
+        self.director = []
+        self.actors = []
+        self.rating = []
+
+        for i in range(self.movieCnt):
+            self.title.append(self.Alldata['items'][i]['title'].strip('</b>').replace('<b>','').replace('</b>',''))
+            self.naverlink.append(self.Alldata['items'][i]['link'])
+            self.image.append(self.Alldata['items'][i]['image'])
+            self.date.append(self.Alldata['items'][i]['pubDate'])
+            self.director.append(self.Alldata['items'][i]['director'].split('|')[0])
+            self.actors.append(self.Alldata['items'][i]['actor'].replace('|', ', '))
+            self.rating.append(float(self.Alldata['items'][i]['userRating']))
+        self.showTitle()
+
+    def showTitle(self):
+        for i in range(self.movieCnt):
+            self.movieListbox.insert(i, self.title[i])
+
+    def showInfo(self):
+        self.indexInfo = self.movieListbox.curselection()[0]
+        self.labelDate.config(text=self.date[self.indexInfo])
+        self.labelDirector.config(text=self.director[self.indexInfo])
+        strLen = len(self.actors[self.indexInfo])
+        if strLen > 17:
+            begStr = self.actors[self.indexInfo][0:17]
+            midStr = self.actors[self.indexInfo][17:]
+            self.labelActors.config(text=begStr)
+            self.labelActors2.config(text=midStr)
+        else:
+            self.labelActors.config(text=self.actors[self.indexInfo])
+            self.labelActors2.config(text=' ')
+
+        self.labelRate.config(text=self.rating[self.indexInfo])
+
+        # 네이버로 열기
+        self.linkL = Label(self.mainWnd, text='네이버로 열기', cursor='hand2')
+        self.linkL.pack()
+        self.linkL.place(x=320,y=180)
+        self.linkL.bind("<Button-1>", lambda e: self.callback(self.naverlink[self.indexInfo]))
+
+        # 관련 뉴스 - 네이버 openAPI 읽어오기
+        client_id = "tvo5aUWG9rwBq1YRMqyJ"
+        client_secret = "40VkT1fuAS"
+        header_parms ={"X-Naver-Client-Id":client_id,"X-Naver-Client-Secret":client_secret}
+        search_word = self.title[self.indexInfo] #검색어
+        encode_type = 'json' #출력 방식 json 또는 xml
+        max_display = 3 #출력 뉴스 수
+        sort = 'sim' #결과값의 정렬기준 시간순 date, 관련도 순 sim
+        start = 1 # 출력 위치
+
+        url = f"https://openapi.naver.com/v1/search/news.{encode_type}?query={search_word}&display={str(int(max_display))}&sort={sort}"
+        res=requests.get(url,headers=header_parms)
+        datas = res.json()
+        links = datas['items']
+        self.link = []
+        for i in links:
+            self.link.append(i['link'])
+        for i in range(max_display):
+            string = '관련뉴스 ' + str(i+1)
+            self.linkL = Label(self.mainWnd, text=string, cursor='hand2')
+            self.linkL.pack()
+            self.linkL.place(x=410,y=120 + i * 30)
+            self.linkL.bind("<Button-1>", lambda e: self.callback(self.link[i]))
 
 
+        # 영화진흥회 openAPI 읽어오기
+        dayOfficeURL = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json?key=edfd0508a0320efa8abbe1eeba097a94&movieNm="
+        dayOfficeURL += self.title[self.indexInfo]
+        res = requests.get(dayOfficeURL)
+        text = res.text
+        d = json.loads(text)
+        code = []
+        name = []
+        genre = []
+        for b in d['movieListResult']['movieList']:
+            #if b['movieNm'] == self.title[self.indexInfo]:
+            code.append(b['openDt'])
+            name.append(b['movieNm'])
+            genre.append(b['genreAlt'])
+        self.labelGenre.config(text=genre)
 
 
-        # 이미지
-        #global photo
-        #photo = PhotoImage(file='movie0.png', master=mainWnd)
-        #photoL = Label(mainWnd, image=photo)
-        #photoL.pack()
-        #photoL.place(x=0,y=0)
+        # 영화 이미지 띄우기
+        if len(self.image) == 0:
+            return
+
+        url = self.image[self.indexInfo]
+        with urllib.request.urlopen(url) as u:
+            raw_data=u.read()
+
+        im=Image.open(BytesIO(raw_data))
+        global image2
+        image2=ImageTk.PhotoImage(im, master=self.mainWnd)
+
+        imgL = Label(self.mainWnd,height=100,width=100)
+        imgL.pack()
+        imgL.place(x=200,y=100)
+        imgL.config(image=image2)
+
+    def callback(self, url):
+        webbrowser.open_new(url)
+
+BoxOfficeRank(frameBoxOffice)
+
+
 
 mainWnd.mainloop()
 
